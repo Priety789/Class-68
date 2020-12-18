d2548f63d301466773c867670ca8ce2f689a492a
Priety789 <65825660+Priety789@users.noreply.github.com>
2020-12-17 21:16:08 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Facebook from './screens/fb';
+import Instagram from './screens/in';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <AppContainer/>
+            </View>
+        );
+    }
+}
+
+var AppNavigator = createSwitchNavigator({
+    Facebook: Facebook,
+    Instagram: Instagram,
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+const TabNavigator = createBottomTabNavigator({
+    FB: { screen: Facebook },
+    IN: { screen: Instagram }
+});
+
+const appContainer = createAppContainer(TabNavigator);
